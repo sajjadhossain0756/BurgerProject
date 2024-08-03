e4cd6bd30d3d92cce0e24ddc74a897bc8c37a826
sajjadhossain <sajjadhossain0756@gmail.com>
2024-08-03 19:26:33 +0600

    menu section add

--- main.js
+++ main.js
@@ -15,3 +15,44 @@ hamburger.addEventListener("click",()=>{
 closeIcon.addEventListener("click",()=>{
     navMenu.classList.add("hide");
 })
+
+// tab function start
+const tabs = document.querySelectorAll(".tab-menu li");
+const all = document.querySelectorAll(".menu-card");
+const food = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snacks");
+const bevarage = document.querySelectorAll(".bevarage");
+ 
+   tabs.forEach((tab)=>{
+        tab.addEventListener("click",()=>{
+            tabs.forEach((tab)=>{
+                 tab.classList.remove("active");
+            })
+
+            tab.classList.add("active");
+
+            const tabval = tab.getAttribute("data-tabs");
+            
+            all.forEach((item)=>{
+                item.style.display = "none" ;
+            })
+            if(tabval == "food"){
+               food.forEach((item)=>{
+                   item.style.display = "block" ;})
+            }
+            else if(tabval == "snacks"){
+               snacks.forEach((item)=>{
+                   item.style.display = "block" ;})
+            }
+            else if(tabval == "bevarage"){
+               bevarage.forEach((item)=>{
+                   item.style.display = "block" ;})
+            }
+            else{
+               all.forEach((item)=>{
+                   item.style.display = "block" ;
+               })
+            }
+
+        })
+   })
